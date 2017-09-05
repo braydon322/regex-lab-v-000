7c441f2b325dd5a04dd68314ab9fe920ee41e632
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-05 22:39:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,9 +2,8 @@ def starts_with_a_vowel?(word)
   return true if word.match(/\b[aeiouAEIOU]/) else false
 end
 
-
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.match(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
